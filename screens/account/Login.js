51937350f51922022f96fac92668ecfb6ebbda45
yilmarcarva <cyilmar6@gmail.com>
2021-03-06 17:50:02 -0500

    Create call to register user

--- screens/account/Login.js
+++ screens/account/Login.js
@@ -1,12 +1,58 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image } from 'react-native'
+import { StyleSheet, Text, View,ScrollView } from 'react-native'
+import { Divider } from 'react-native-elements'
 
 export default function Login() {
     return (
-        <View>
-            <Text>Login.....</Text>
-        </View>
+        <ScrollView>
+            <Image
+                source={require("../../assets/restaurant-logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <View style={styles.container}>
+                <Text>Login Form</Text>
+                <CreateAccount/>
+            </View>
+            <Divider style={styles.divider}/>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+function CreateAccount (props){
+    return (
+        <Text style={styles.register}
+            onPress={() => console.log("Me voy a registrar hpta")}    
+        >
+            ¿Aún no tienes cuenta? {""}
+            <Text style={styles.btnRegistrar}>
+                Regístrate
+            </Text>
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        height:130,
+        width:"40%",
+        marginBottom:20        
+    },
+    container: {
+        marginHorizontal: 40
+    },
+    divider: {
+        backgroundColor: "#442484",
+        margin: 40
+    },
+    register: {
+        marginTop:15,
+        marginHorizontal: 10,
+        alignSelf: "center"
+    },
+    btnRegistrar: {
+        color: "#442484",
+        fontWeight:"bold"
+    }
+})
