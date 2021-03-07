7099810a60d2ac84e7334dc9c2039ef123891719
yilmarcarva <cyilmar6@gmail.com>
2021-03-06 19:23:20 -0500

    method save new user

--- utils/components/account/RegisterForm.js
+++ utils/components/account/RegisterForm.js
@@ -1,34 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Input,Button } from 'react-native-elements'
+import { Input,Button, Icon } from 'react-native-elements'
 
 
 export default function RegisterForm() {
+    const [showPassword, setshowPassword] = useState(false)
+    const [showPasswordConfirm, setPasswordConfirm] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+
+
+    const onChange =(e, type) =>{
+        setFormData({...formData, [type]: e.nativeEvent.text})
+    }
+
     return (
         <View style={styles.form}>
         <Input
             containerStyle={styles.input}
             placeholder="Ingresa tu E-mail"
+            onChange={(e) => onChange(e,"email")}    
+            rightIcon={<Icon
+                type="material-community"
+                name="at"
+                iconStyle={styles.icon}
+                />}
+            keyboardType="email-address"
         />
         <Input
             containerStyle={styles.input}
             placeholder="Ingresa tu Contraseña"
             password={true}
-            secureTextEntry={true}
+            secureTextEntry={!showPassword}
+            onChange={(e) => onChange(e,"password")}
+            rightIcon={<Icon
+                type="material-community"
+                name={showPassword ? "eye-off-outline" :"eye-outline"}
+                iconStyle={styles.icon}
+                onPress={() => setshowPassword(!showPassword)}
+                />}
+            
         />
         <Input
             containerStyle={styles.input}
             placeholder="Confirmar Contraseña"
             password={true}
-            secureTextEntry={true}
+            secureTextEntry={!showPasswordConfirm}
+            onChange={(e) => onChange(e,"confirm")}
+            rightIcon={<Icon
+                type="material-community"
+                name={showPasswordConfirm ? "eye-off-outline" :"eye-outline"}
+                iconStyle={styles.icon}
+                onPress={() => setPasswordConfirm(!showPasswordConfirm)}
+                />}
+            
         />
         <Button
             title="Registrar Usuario"
             buttonStyle={styles.button}
             containerStyle={styles.btnContainer}
+            onPress={()=> console.log(formData)}
         />
         </View>
     )
+}
+
+const defaultFormValues = () =>{
+    return { email: "", password:"", confirm:""}
 }
 
 const styles = StyleSheet.create({
@@ -48,6 +85,9 @@ const styles = StyleSheet.create({
         width: "80%",
         alignSelf: "center",
         borderRadius: 10
+    },
+    icon: {
+        color: "#c1c1c1"
     }
 
 })
